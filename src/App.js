a45437f0d6f2d0f55c1b4ea41b1dee7b4a162868
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-09-08 01:21:39 +0600

    react-router-dom added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, Routes } from "react-router-dom";
+import BookList from "./pages/BookList";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/booklist">BookList</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        {/* path="/" means that it is the main root */}
+        <Route path="/" element={<Home />} />
+        <Route path="/booklist" element={<BookList />} />
+      </Routes>
+    </>
   );
 }
 
