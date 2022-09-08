b6749cccc1651d046d7bba9271fa41a25cd839b7
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-09-08 13:35:20 +0600

    routerv6

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
+
+// We can use navigate for directly redirecting another page
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  // return <div>NotFound</div>;
+  return (
+    <>
+      <Navigate to="/" />
+    </>
+  );
 };
 
 export default NotFound;
