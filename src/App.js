3257f06db1cee64f8baf261fb36b0c893c19a5cc
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-09-08 13:32:00 +0600

    Routes using navLink instead of Link

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import BookRoutes from "./BookRoutes";
 import Book from "./pages/Book";
+import "./styles.css";
 
 function App() {
   // Routes using hooks
-  let element = useRoutes([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "*",
-      element: <NotFound />,
-    },
-    {
-      path: "/books/*",
-      element: <BookRoutes />,
-    },
-  ]);
+  // let element = useRoutes([
+  //   {
+  //     path: "/",
+  //     element: <Home />,
+  //   },
+  //   {
+  //     path: "*",
+  //     element: <NotFound />,
+  //   },
+  //   {
+  //     path: "/books/*",
+  //     element: <BookRoutes />,
+  //   },
+  // ]);
 
   return (
     <>
@@ -37,27 +38,33 @@ function App() {
           </li>
         </ul>
       </nav>
-      {element}
+
+      {/* {element} */}
+
       {/* Routes using JSX */}
-      {/* <Routes>
+
+      <Routes>
         {/* path="/" means that it is the main root */}
-      {/* element means which page to generate */}
-      {/* we generating all the routes here. so that we can use them in different component */}
-      {/* <Route path="/" element={<Home />} /> */}
-      {/* Nesting Route components */}
-      {/* <Route path="/books/*" element={<BookRoutes />} /> */}
-      {/* index means it will use the parent route which is /books */}
-      {/* <Route index element={<BookList />} />
+        {/* element means which page to generate */}
+        {/* we generating all the routes here. so that we can use them in different component */}
+        <Route path="/" element={<Home />} />
+
+        {/* Nesting Route components */}
+        <Route path="/books/*" element={<BookRoutes />} />
+        {/* index means it will use the parent route which is /books */}
+        {/* <Route index element={<BookList />} />
           <Route path=":id" element={<Book />} />
           <Route path="new" element={<NewBook />} />
         </Route> */}
-      {/* These route are not nestied */}
-      {/* <Route path="/books" element={<BookList />} />
+
+        {/* These route are not nestied */}
+        {/* <Route path="/books" element={<BookList />} />
         <Route path="/books/:id" element={<Book />} />
         <Route path="/books/new" element={<NewBook />} /> */}
-      {/* * used for identifying any vogus params in the url  */}
-      {/* <Route path="*" element={<NotFound />} /> */}
-      {/* </Routes> */}
+
+        {/* * used for identifying any vogus params in the url  */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
--- src/BookLayout.js
+++ src/BookLayout.js
@@ -1,14 +1,37 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 
 const BookLayout = () => {
   return (
     <>
-      <Link to="/books/1">Book1</Link>
+      {/* Link has three attributes
+          1.Replace = it will replace the previous page(mainly used in login/logout)
+          2.reloadDocument = it will reload the whole doucment
+          3.state = using this we can send data to another route */}
+
+      {/* <Link to="/books/1">Book1</Link>
       <br />
       <Link to="/books/2">Book2</Link>
       <br />
-      <Link to="/books/new">Book New</Link>
+          <Link to="/books/new">Book New</Link> */}
+
+      {/* NavLink provide us three properties
+          1.style = it will style the link using isActive
+          2.class = 
+          3.children */}
+
+      <NavLink
+        style={({ isActive }) => {
+          return isActive ? { color: "red" } : null;
+        }}
+        to="/books/1"
+      >
+        Book1
+      </NavLink>
+      <br />
+      <NavLink to="/books/2">Book2</NavLink>
+      <br />
+      <NavLink to="/books/new">Book New</NavLink>
       {/* Outlet will render the current route */}
       <Outlet context={{ hello: "I am sourav" }} />
     </>
